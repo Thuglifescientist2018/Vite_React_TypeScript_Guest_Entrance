52a985c3a164116d6c9e480857f50007a21b99b4
Shashwat Dahal <thuglifescientist2018@gmail.com>
2025-08-05 10:19:34 +0545

    get People and render People through getPeople() and setPeople()

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
 
 
 
+
 import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [sum, setSum] = useState<number>(0)
- const sumSetter = (a:number, b:number):number => {
-  setSum(a+b)
-  return a + b
- }
- useEffect(() => {
-  sumSetter(1,1)
- }, [])
-
+  interface Person {
+    name: {
+      first: string
+    }
+  }
+  const [people, setPeople] = useState<Person[]>([]);
+  const getPeople = async():Promise<Person[]> => {
+    const response = await fetch("https://randomuser.me/api/?results=5");
+    const data: {results: Person[] } = await response.json();
+    const people:Person[]  = data.results;
+    return people;
+  }
+  useEffect(() => {
+    getPeople().then(people => setPeople(people))
+  },[])
   return (
     <>
-      <h1>Hello from Shashwat {sum} </h1>
+    <button onClick={() =>     getPeople().then(people => setPeople(people)) }>Refresh</button>
+      <h1>Guest Entrance </h1>
+      {people.length && people.map(person => (
+        <li>
+          {person.name.first}
+        </li>
+      ))}
     </>
   )
 }
