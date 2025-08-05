9f4ce2a5de7a1ca023a43832ea134cdf57b06037
Shashwat Dahal <thuglifescientist2018@gmail.com>
2025-08-05 17:11:19 +0545

    add searchField filter functionality

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,27 @@ function App() {
   interface Person {
     name: {
       first: string
+    },
+    picture: {
+      large: string
     }
   }
   const [people, setPeople] = useState<Person[]>([]);
+  const [searchField, setSearchField] = useState<string>('')
   const getPeople = async():Promise<Person[]> => {
     const response = await fetch("https://randomuser.me/api/?results=5");
     const data: {results: Person[] } = await response.json();
     const people:Person[]  = data.results;
     return people;
   }
+  const onSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchField(event.target.value)
+  }
+  const filterPeople = (people: Person[], searchField:string): Person[] => {
+    const filteredPeople:Person[] = people.filter((person: Person) => person.name.first.toLowerCase().includes(searchField.toLowerCase()));
+    return filteredPeople
+  }
+  const filteredPeople = filterPeople(people, searchField)
   useEffect(() => {
     getPeople().then(people => setPeople(people))
   },[])
@@ -25,9 +37,15 @@ function App() {
     <>
     <button onClick={() =>     getPeople().then(people => setPeople(people)) }>Refresh</button>
       <h1>Guest Entrance </h1>
-      {people.length && people.map(person => (
+      <input type="text" placeholder='search guests' onChange={onSearchChange} />
+    
+      {filteredPeople.length && filteredPeople.map((person:Person) => (
         <li>
+          <img src={person.picture.large} alt="Person's profile picture" />
+          <h4>
+
           {person.name.first}
+          </h4>
         </li>
       ))}
     </>
